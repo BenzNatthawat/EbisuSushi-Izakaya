a82683894e032fb7ebe9f4a44f5ef36ec601c801
Banz Singkala <you@example.com>
2019-09-06 15:49:41 +0700

    6/9v.1

--- layouts/header.php
+++ layouts/header.php
@@ -6,16 +6,10 @@
           <a href="#"><i class="fa fa-phone"></i>093-690-0670</a>
         </div>
       </div>
-      <div class="col-md-4">
-        <div class="event_btn_inner">
-          <a class="event_btn" href="table.html"><i class="fa fa-table" aria-hidden="true"></i>Book a Table</a>
-          <a class="event_btn" href="event.html"><i class="fa fa-calendar" aria-hidden="true"></i>Book an Event</a>
-        </div>
-      </div>
-      <div class="col-md-4 align-self-end">
+      <div class="col-md-8 align-self-end">
         <div class="header_social">
           <ul>
-            <li><a href="https://www.facebook.com/EbisuHatyai/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://www.facebook.com/EbisuHatyai"><i class="fa fa-facebook"></i></a></li>
             <li><a href="https://www.instagram.com/ebisuhatyai"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
